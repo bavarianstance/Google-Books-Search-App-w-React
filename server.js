d8814308917eb61d4948c29c3e7d66e9bee94c09
bavarianstance <bavarianstance@gmail.com>
2019-04-27 08:24:40 -0700

    finish init config and models

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true})
+  .then(() => {
+    console.log("🗄 ==> Successfully connected to mongoDB.");
+  })
+  .catch((err) => {
+    console.log(`Error connecting to mongoDB: ${err}`);
+  });
+
+require("./routes/api-routes")(app);
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
