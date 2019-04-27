e405aab8b61c5598e70b47ef95c7b8beea7cd5e2
bavarianstance <bavarianstance@gmail.com>
2019-04-27 08:51:05 -0700

    add bootstrap and utils api

---
+++ client/src/utils/API.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default {
+	getBooks: () => {
+		return axios.get("api/books");
+	},
+	searchBooks: (query) => {
+		return axios.post("/search", {title: query});
+	},
+	saveBook: (data) => {
+		return axios.post("/api/books", data);
+	},
+	deleteBook: (id) => {
+		return axios.delete(`/api/books/${id}`);
+	}
+}
