77b4d7bc7dd591d4eed249c318df3898c554c494
bavarianstance <bavarianstance@gmail.com>
2019-04-27 08:43:00 -0700

    finish api routes

---
+++ routes/api_routes.js
@@ -0,0 +1,46 @@
+const axios = require("axios");
+const db = require("../models");
+const path = require("path");
+
+module.exports = function(app) {
+	app.get("/api/books", (request, response) => {
+		db.Book.find().then(
+			(data) => {
+				response.json(data);
+			}).catch((err) => {
+				response.json({error: err});
+			});
+	});
+
+	app.post("/search", (request, response) => {
+		let search = request.body.title.replace(/\s/g, "+");
+		axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=15`)
+		.then((result) => {
+			response.json(result.data.items)
+		}).catch((err) => {
+			response.json({error: err});
+		});
+	});
+
+	app.post("/api/books", (request, response) => {
+		db.Book.create(request.body).then(
+			(data) => {
+				response.json({successful: data});
+			}).catch ((err) => {
+				response.json({error:err});
+			});
+	});
+
+	app.delete("/api/books/:id", (request, response) => {
+		db.Book.findByIdAndDelete(request.params.id).then(
+			(data) => {
+				response.json({successful:data});
+			}).catch((err) => {
+				response.json({error: err});
+			});
+	});
+
+	app.get("*", (request, response) => {
+		response.sendFile(path.join(__dirname, "../client/build/index.html"));
+	});
+}
